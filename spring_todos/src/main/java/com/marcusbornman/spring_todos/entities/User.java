b37b99b557dcc363868bb585fcfd59a52cea19a6
marcus-bornman <marcus.bornman@gmail.com>
2021-01-25 14:59:29 +0200

    Removed UserDetails entity in favour of just using User Entity.

--- spring_todos/src/main/java/com/marcusbornman/spring_todos/entities/User.java
+++ spring_todos/src/main/java/com/marcusbornman/spring_todos/entities/User.java
@@ -1,19 +1,26 @@
-package com.marcusbornman.spring_todos.model;
+package com.marcusbornman.spring_todos.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @NotNull
     @NotEmpty
@@ -25,25 +32,30 @@ public class User {
     private String password;
 
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime expiryDate = LocalDateTime.of(9999, 12, 31, 23, 59, 59);
 
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Boolean locked = false;
 
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime credentialsExpiryDate = LocalDateTime.of(9999, 12, 31, 23, 59, 59);
 
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Boolean disabled = false;
 
     @CreationTimestamp
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime creationDate;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private List<Role> roles;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private List<TodoList> todoLists;
 
     public void setPassword(String password) {
@@ -51,5 +63,35 @@ public class User {
             BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             this.password = passwordEncoder.encode(password);
         }
+    }
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return LocalDateTime.now().isBefore(expiryDate);
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return LocalDateTime.now().isBefore(credentialsExpiryDate);
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return !disabled;
     }
 }
