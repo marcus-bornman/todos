2b4fc2072a254a4e32c24738a02438789b7d1893
marcus-bornman <marcus.bornman@gmail.com>
2021-01-30 16:51:24 +0200

    Implemented singleton

--- flutter_todos/lib/repos/todo_list_repo/todo_list_repo.dart
+++ flutter_todos/lib/repos/todo_list_repo/todo_list_repo.dart
@@ -1,6 +1,15 @@
 import 'package:flutter_todos/models/todo_list/todo_list.dart';
+import 'package:flutter_todos/repos/todo_list_repo/firebase_todo_list_repo.dart';
 
 abstract class TodoListRepo {
+  static TodoListRepo _instance;
+
+  static get instance {
+    if (_instance == null) _instance = FirebaseTodoListRepo();
+
+    return _instance;
+  }
+
   Future<void> addNewTodoList(TodoList todo);
 
   Future<void> deleteTodoList(TodoList todo);
